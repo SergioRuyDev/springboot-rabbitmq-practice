986b1141133b4bb238056dbb440fcd216d2effdc
sergioruydev <sergioruyenator@gmail.com>
2023-01-31 10:21:07 -0300

    Initial commit

---
+++ src/main/java/com/sergioruy/springboot/SpringbootRabbitmqPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sergioruy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRabbitmqPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergioruy/springboot/SpringbootRabbitmqPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergioruy.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
