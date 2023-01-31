023fd0cb2db174e3ea340d9ab17adb5b7c63589d
sergioruydev <sergioruyenator@gmail.com>
2023-01-31 11:34:42 -0300

    Producer created.

---
+++ src/main/java/com/sergioruy/springboot/publisher/RabbitMQProducer.java
@@ -0,0 +1,31 @@
+package com.sergioruy.springboot.publisher;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQProducer.class);
+
+    private RabbitTemplate rabbitTemplate;
+
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+
+    public void sendMessages(String message) {
+        LOGGER.info("Send Message -> {}", message);
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    }
+}
